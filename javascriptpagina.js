fd0527d0b0245aeb1b5b7ff4dba4696416da2e41
Luis David Sáenz Jurado <luisdavidspider@gmail.com>
2020-03-15 18:23:00 -0600

    Código js agregado, y algunas funciones extra
    Código js agregado, y algunas funciones y ejemplos extra, manipular objetos DOM, manipulación de objetos de entrada HTML, etc.

--- javascriptpagina.js
+++ javascriptpagina.js
@@ -8,4 +8,45 @@ $(document).ready(function() {
     $("#id02").remove();
   });
 
-  
+function verURL() {
+  document.getElementById("url").innerHTML = document.URL
+}
+
+function verDominio() {
+  document.getElementById("dominio").innerHTML = document.domain;
+}
+
+function verFechaMod() {
+  document.getElementById("fechaMod").innerHTML = document.lastModified;
+}
+
+function verTitulo() {
+  document.getElementById("tituloDoc").innerHTML =
+"El títilo de este documento es: " + document.title;
+}
+
+function reemplazarContenido() {
+  document.open("patio","replace");
+  document.write("<h2>Felicidades, Cambiaste el contenido, recarga la página para volver.</h2>");
+  document.close();
+}
+
+function inhabilitarElemento() {
+  document.getElementById("boton2").disabled = true;
+}
+
+function reiniciarValores() {
+  document.getElementById("frm1").reset();
+}
+
+function inhabilitarLista() {
+  document.getElementById("miSeleccion").disabled=true;
+}
+function habilitarLista() {
+  document.getElementById("miSeleccion").disabled=false;
+}
+
+function cambiarTextoMayus() {
+  var x = document.getElementById("pnombre");
+  x.value = x.value.toUpperCase();
+}
